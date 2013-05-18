bac0edf4cf4306de256b22b88a991266dd4676bf
Jeff Hays <jeff@offthewallmedia.com>
2013-05-18 02:44:17 -0700

    okay, might as well space out all my { too

--- sqlbldr.php
+++ sqlbldr.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 
 // Jeff Hays' BLDR class <3
 
-class db extends stdClass{
+class db extends stdClass {
 	// Default credentials
 	private $dbhost = "localhost";
 	private $db = "test";
@@ -28,18 +28,18 @@ class db extends stdClass{
 	static $delete;
 	
 	// Construct
-	public function __construct(){
+	public function __construct() {
 		$this->connect($this->dbhost, $this->dbuser, $this->dbpass, $this->db);
 		$this->table = $this->columns = $this->values = $this->where = $this->order = $this->sql = false;
 	}
 	
 	// Select instance
-	public static function select($arg=false){
+	public static function select($arg=false) {
 		// Setup or reset instance
 		$c = __CLASS__;
 		self::$select = new $c;
 
-		if(is_array($arg) || $arg == false){
+		if(is_array($arg) || $arg == false) {
 			// Array input (better on resources)
 			self::$select->columns = (!$arg) ? "*" : implode(', ', $arg);
 		} else {
@@ -52,7 +52,7 @@ class db extends stdClass{
 	}
 
 	// Update instance
-	public static function update($arg){
+	public static function update($arg) {
 		// Setup or reset instance
 		$c = __CLASS__;
 		self::$update = new $c;
@@ -64,12 +64,12 @@ class db extends stdClass{
 	}
 
 	// Insert instance
-	public static function insert($table=false, $columns=false){
+	public static function insert($table=false, $columns=false) {
 		// Setup or reset instance
 		$c = __CLASS__;
 		self::$insert = new $c;
 
-		if(is_array($columns) && count($columns) > 0){
+		if(is_array($columns) && count($columns) > 0) {
 			// Array of columns passed
 			self::$insert->table = $table;
 			self::$insert->columns = $columns;
@@ -83,7 +83,7 @@ class db extends stdClass{
 	}
 
 	// Delete instance
-	public static function delete($arg=false){
+	public static function delete($arg=false) {
 		$c = __CLASS__;
 		self::$delete = new $c;
 		self::$delete->table = $arg ? "`$arg`" : false;
@@ -92,18 +92,18 @@ class db extends stdClass{
 	}
 	
 	// Connect
-	public function connect($dbhost=false, $dbuser=false, $dbpass=false, $db=false){
+	public function connect($dbhost=false, $dbuser=false, $dbpass=false, $db=false) {
 		if(!$dbhost) $dbhost = $this->dbhost;
 		if(!$dbuser) $dbuser = $this->dbuser;
 		if(!$dbpass) $dbpass = $this->dbpass;
 		if(!$db) $db = $this->db;
-		if($dbhost && $dbuser && $dbpass && $db){
+		if($dbhost && $dbuser && $dbpass && $db) {
 			$this->conn = mysql_connect($dbhost, $dbuser, $dbpass);
 			if(!$this->conn) die("Could not connect to $dbhost");
 			$this->log .= "Connected Successfully!\n";
-			try{
+			try {
 				mysql_select_db($db);
-			}catch(exception $err){
+			} catch(exception $err) {
 				echo $err;
 			}
 			$this->log .= "Successfully selected database: $db\n";
@@ -111,9 +111,9 @@ class db extends stdClass{
 	}
 
 	// Disconnect
-	public function disconnect(){
-		if($this->conn){
-			if(@mysql_close($this->conn)){
+	public function disconnect() {
+		if($this->conn) {
+			if(@mysql_close($this->conn)) {
 				return true;
 			} else {
 				return false;
@@ -122,30 +122,30 @@ class db extends stdClass{
 	}
 
 	// Change DB (with same creds)
-	public function setdb($db){
-		if($this->isdb($db)){
-			try{
+	public function setdb($db) {
+		if($this->isdb($db)) {
+			try {
 				mysql_select_db($db);
-			}catch(exception $err){
+			} catch(exception $err) {
 				echo $err;
 			}
 			$this->log .= "Successfully selected database: $db\n";
 		}
-		if(self::$select->columns){
+		if(self::$select->columns) {
 			return self::$select;
-		} else if(self::$insert->columns){
+		} else if(self::$insert->columns) {
 			return self::$insert;
 		}
 	}
     
 	// Plain ol' query
-	public function query($str){
+	public function query($str) {
 		return mysql_query($str, $this->conn);
 	}
 
 	// From tables (select only)
-	public function from($args=false, $moreargs=false){
-		if(self::$select->columns){
+	public function from($args=false, $moreargs=false) {
+		if(self::$select->columns) {
 		  // Allow both array or comma delimited input (array uses less resources)
 		  if(!$moreargs && !is_array($args)) $args = array($args);
 			$args = $moreargs ? func_get_args() : $args;
@@ -155,13 +155,13 @@ class db extends stdClass{
 		return self::$select;
 	}
 
-	public function set($args){
-		if(self::$update->table && is_array($args) && count($args) > 0){
-			if(array_keys($args) !== range(0, count($args) - 1)){
+	public function set($args) {
+		if(self::$update->table && is_array($args) && count($args) > 0) {
+			if(array_keys($args) !== range(0, count($args) - 1)) {
 				// Associative array passed with values
 				self::$update->sql .= ' SET';
 				$c = '';
-				foreach($args as $k=>$val){
+				foreach($args as $k=>$val) {
 					$val = $this->sanitize($val);
 					self::$update->columns[] = $k;
 					self::$update->sql .= is_numeric($val) ? "$c `$k` = $val" : "$c `$k` = '$val'";
@@ -174,14 +174,14 @@ class db extends stdClass{
 		return self::$update;
 	}
 
-	public function values($args=false){
-		if(self::$insert->table){
+	public function values($args=false) {
+		if(self::$insert->table) {
 			// Insert
-			if(is_array($args)){
+			if(is_array($args)) {
 				// Array of values
-				if(array_keys($args) !== range(0, count($args) - 1)){
+				if(array_keys($args) !== range(0, count($args) - 1)) {
 					// Associative array passed ('columnName' => 'value')
-					if(self::$insert->columns){
+					if(self::$insert->columns) {
 						$this->log .= "Error: Insert columns already set - don't pass columns in insert() when using associative array in values()\n";
 					} else {
 						// Set columns
@@ -202,22 +202,22 @@ class db extends stdClass{
 		}
 	}
 
-	public function where($str=false, $operand=false, $condition=false){
+	public function where($str=false, $operand=false, $condition=false) {
 		// Initialize temp variables for string building
 		$tmpwhere = $tmpsql = '';
-		if($str && $operand && $condition){
+		if($str && $operand && $condition) {
 			// Start where
-			if(is_object(self::$select) || is_object(self::$update) || is_object(self::$delete)){
+			if(is_object(self::$select) || is_object(self::$update) || is_object(self::$delete)) {
 				$tmpwhere .= "WHERE `$str` $operand ";
 				$tmpsql .= " WHERE `$str` $operand ";
-				switch(strtoupper($operand)){
+				switch(strtoupper($operand)) {
 					case 'IN':
 					case 'NOT IN':
 						// IN and NOT IN
-						if(is_array($condition)){
+						if(is_array($condition)) {
 							$tmpwhere .= "(";
 							$tmpsql .= "(";
-							foreach($condition as $k=>$c){
+							foreach($condition as $k=>$c) {
 								$tmpwhere .= (is_numeric($c) ? $c : "'" . $this->sanitize($c) . "'") . ($k == count($condition) - 1 ? '' : ', ');
 								$tmpsql .= (is_numeric($c) ? $c : "'" . $this->sanitize($c) . "'") . ($k == count($condition) - 1 ? '' : ', ');
 							}
@@ -232,30 +232,30 @@ class db extends stdClass{
 						break;
 				}
 			}
-		} else if($str){
+		} else if($str) {
 			// Literal string was passed in where()
-			if($this->columns && $this->table){
+			if($this->columns && $this->table) {
 				$tmpwhere .= "WHERE $str";
 				$tmpsql .= " WHERE $str";
 			}
 		}
 		// Return
-		if(is_object(self::$select)){
+		if(is_object(self::$select)) {
 			// Select
 			self::$select->where = $tmpwhere;
 			self::$select->sql .= $tmpsql;
 			return self::$select;
-		} else if(is_object(self::$insert)){
+		} else if(is_object(self::$insert)) {
 			// Insert
 			self::$insert->where = $tmpwhere;
 			self::$insert->sql .= $tmpsql;
 			return self::$insert;
-		} else if(is_object(self::$update)){
+		} else if(is_object(self::$update)) {
 			// Update
 			self::$update->where = $tmpwhere;
 			self::$update->sql .= $tmpsql;
 			return self::$update;
-		} else if(is_object(self::$delete)){
+		} else if(is_object(self::$delete)) {
 			// Delete
 			self::$delete->where = $tmpwhere;
 			self::$delete->sql .= $tmpsql;
@@ -263,22 +263,22 @@ class db extends stdClass{
 		}
 	}
 
-	public function andwhere($str=false, $operand=false, $condition=false){
+	public function andwhere($str=false, $operand=false, $condition=false) {
 		// Initialize temp variables for string building
 		$tmpwhere = $tmpsql = '';
-		if($str && $operand && $condition){
+		if($str && $operand && $condition) {
 			// Start where
-			if(is_object(self::$select) || is_object(self::$update) || is_object(self::$delete)){
+			if(is_object(self::$select) || is_object(self::$update) || is_object(self::$delete)) {
 				$tmpwhere .= "AND `$str` $operand ";
 				$tmpsql .= " AND `$str` $operand ";
-				switch(strtoupper($operand)){
+				switch(strtoupper($operand)) {
 					case 'IN':
 					case 'NOT IN':
 						// IN and NOT IN
-						if(is_array($condition)){
+						if(is_array($condition)) {
 							$tmpwhere .= "(";
 							$tmpsql .= "(";
-							foreach($condition as $k=>$c){
+							foreach($condition as $k=>$c) {
 								$tmpwhere .= (is_numeric($c) ? $c : "'" . $this->sanitize($c) . "'") . ($k == count($condition) - 1 ? '' : ', ');
 								$tmpsql .= (is_numeric($c) ? $c : "'" . $this->sanitize($c) . "'") . ($k == count($condition) - 1 ? '' : ', ');
 							}
@@ -293,30 +293,30 @@ class db extends stdClass{
 						break;
 				}
 			}
-		} else if($str){
+		} else if($str) {
 			// Literal string was passed in where()
-			if($this->columns && $this->table){
+			if($this->columns && $this->table) {
 				$tmpwhere .= "AND $str";
 				$tmpsql .= " AND $str";
 			}
 		}
 		// Return
-		if(is_object(self::$select)){
+		if(is_object(self::$select)) {
 			// Select
 			self::$select->where = $tmpwhere;
 			self::$select->sql .= $tmpsql;
 			return self::$select;
-		} else if(is_object(self::$insert)){
+		} else if(is_object(self::$insert)) {
 			// Insert
 			self::$insert->where = $tmpwhere;
 			self::$insert->sql .= $tmpsql;
 			return self::$insert;
-		} else if(is_object(self::$update)){
+		} else if(is_object(self::$update)) {
 			// Update
 			self::$update->where = $tmpwhere;
 			self::$update->sql .= $tmpsql;
 			return self::$update;
-		} else if(is_object(self::$delete)){
+		} else if(is_object(self::$delete)) {
 			// Delete
 			self::$delete->where = $tmpwhere;
 			self::$delete->sql .= $tmpsql;
@@ -324,9 +324,9 @@ class db extends stdClass{
 		}
 	}
 
-	public function order($cols=false, $direction=false){
-		if(self::$select->columns && self::$select->table){
-			if(is_array($cols) && count($cols) > 0){
+	public function order($cols=false, $direction=false) {
+		if(self::$select->columns && self::$select->table) {
+			if(is_array($cols) && count($cols) > 0) {
 				self::$select->order = (strstr($cols[0], '`')) ? implode(', ', $cols) : '`' . implode('`, `', $cols) . '`';
 			}
 			self::$select->sql .= " ORDER BY " . self::$select->order . " " . (isset($direction) && strlen($direction) > 0 ? $direction : "ASC");
@@ -335,32 +335,32 @@ class db extends stdClass{
 	}
 
 	// This function or run() below are required after insert, update, and delete commands
-	public function execute(){
-		if(is_object(self::$insert) && self::$insert->columns && self::$insert->values){
+	public function execute() {
+		if(is_object(self::$insert) && self::$insert->columns && self::$insert->values) {
 			return $this->query(self::$insert->sql);
-		} else if(is_object(self::$update) && self::$update->columns){
+		} else if(is_object(self::$update) && self::$update->columns) {
 			return $this->query(self::$update->sql);
-		} else if(is_object(self::$delete)){
+		} else if(is_object(self::$delete)) {
 			return $this->query(self::$delete->sql);
 		} 
 	}
 
 	// This function or execute() below are required after insert, update, and delete commands
-	public function run(){
-		if(is_object(self::$insert) && self::$insert->columns && self::$insert->values){
+	public function run() {
+		if(is_object(self::$insert) && self::$insert->columns && self::$insert->values) {
 			return $this->query(self::$insert->sql);
-		} else if(is_object(self::$update) && self::$update->columns){
+		} else if(is_object(self::$update) && self::$update->columns) {
 			return $this->query(self::$update->sql);
-		} else if(is_object(self::$delete)){
+		} else if(is_object(self::$delete)) {
 			return $this->query(self::$delete->sql);
 		} 
 	}
 
 	// Function to clean SQL
-	public function sanitize($args){
-		if(is_array($args) && count($args) > 0){
+	public function sanitize($args) {
+		if(is_array($args) && count($args) > 0) {
 			$sanitized = array();
-			foreach($args as $arg){
+			foreach($args as $arg) {
 	  			$sanitized[] = mysql_real_escape_string($arg);
 			}
 		} else {
@@ -370,12 +370,12 @@ class db extends stdClass{
 	}
 
 	// Export as object
-	public function asobject(){
-		if(self::$select->columns && self::$select->table){
+	public function asobject() {
+		if(self::$select->columns && self::$select->table) {
 			$res = $this->query(self::$select->sql);
 			$obj = false;
-			if($res && mysql_num_rows($res) > 0){
-				while($row = mysql_fetch_object($res)){
+			if($res && mysql_num_rows($res) > 0) {
+				while($row = mysql_fetch_object($res)) {
 					$obj[] = $row;
 				}
 			}
@@ -384,12 +384,12 @@ class db extends stdClass{
 	}
 
 	// Export as array
-	public function asarray(){
-		if(self::$select->columns && self::$select->table){		
+	public function asarray() {
+		if(self::$select->columns && self::$select->table) {		
 			$res = $this->query(self::$select->sql);
 			$assoc = false;
-			if($res && mysql_num_rows($res) > 0){
-				while($row = mysql_fetch_assoc($res)){
+			if($res && mysql_num_rows($res) > 0) {
+				while($row = mysql_fetch_assoc($res)) {
 					$assoc[] = $row;
 				}
 			}
@@ -400,8 +400,8 @@ class db extends stdClass{
 	}
 
 	// Export as .csv (must run this in the header before any information is printed to the browser)
-	public function ascsv($fname=false){
-		if(self::$select->table && self::$select->columns){
+	public function ascsv($fname=false) {
+		if(self::$select->table && self::$select->columns) {
 			// Initialize
 			$csv = '';
 			$fname = ($fname && strpos($fname, ".csv") === false) ? $fname .= ".csv" : 'output.csv';
@@ -409,25 +409,25 @@ class db extends stdClass{
 			// Execute current select SQL and set associative array
 			$res = $this->query(self::$select->sql);
 			$assoc = false;
-			if($res && mysql_num_rows($res)>0){
-				while($row = mysql_fetch_assoc($res)){
+			if($res && mysql_num_rows($res)>0) {
+				while($row = mysql_fetch_assoc($res)) {
 					$assoc[] = $row;
 				}
 			}
 
 			// Loop through results and build records and headings
-			if(is_array($assoc) && count($assoc) > 0){
+			if(is_array($assoc) && count($assoc) > 0) {
 				$heading = '"' . implode('","', array_keys($assoc[0])) . '"';
-				foreach($assoc as $col=>$row){
-					foreach($row as $key=>$val){
+				foreach($assoc as $col=>$row) {
+					foreach($row as $key=>$val) {
 						$records[$col][] = $val;
 					}
 				}
 
 				// Add heading and records to CSV output
 				$csv .= $heading . "\r\n";
-				foreach($records as $record){
-					foreach($record as $k=>$r){
+				foreach($records as $record) {
+					foreach($record as $k=>$r) {
 						$csv .= '"' . $r . '"' . ($k < count($record) - 1 ? ',' : '');
 					}
 					$csv .= "\r\n";
@@ -444,10 +444,10 @@ class db extends stdClass{
 	}
 
 	// Check if a db exists
-	public function isdb($db){
+	public function isdb($db) {
 		$dbresult = @mysql_query("SHOW DATABASES LIKE '$db'");
-		if($dbresult){
-			if(mysql_num_rows($dbresult) == 1){
+		if($dbresult) {
+			if(mysql_num_rows($dbresult) == 1) {
 				return true;
 			} else {
 				return false;
@@ -456,10 +456,10 @@ class db extends stdClass{
 	}
 
 	// Check if a table exists
-	public function istable($table){
+	public function istable($table) {
 		$dbtables = @mysql_query("SHOW TABLES FROM '{$this->db}' LIKE '{$table}'");
-		if($dbtables){
-			if(mysql_num_rows($dbtables) == 1){
+		if($dbtables) {
+			if(mysql_num_rows($dbtables) == 1) {
 				return true;
 			} else {
 				return false;
@@ -470,11 +470,11 @@ class db extends stdClass{
 // Legacy functions
 
 	// Run query and return associative array
-	public function assoc($str){
+	public function assoc($str) {
 		$res = $this->query($str);
 		$assoc = false;
-		if($res && mysql_num_rows($res)>0){
-			while($row = mysql_fetch_assoc($res)){
+		if($res && mysql_num_rows($res)>0) {
+			while($row = mysql_fetch_assoc($res)) {
 				$assoc[] = $row;
 			}
 		}
